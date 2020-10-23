3e487aa64bcc4aa2704332251ad47a596c5e0a99
Jamie Cansdale <jcansdale@github.com>
2020-10-23 11:13:14 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+group = "com.octokat.app"
+version = "1.0-SNAPSHOT"
+plugins {
+    application
+}
+application {
+    mainClass.set("com.octokat.app.InstallApp")
+}
+repositories {
+    jcenter()
+    maven {
+        name = "remote"
+        url = uri("https://maven.pkg.github.com/${{ github.repository_owner }}/maven-publish")
+        credentials {
+            username = "token"
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
+}
+dependencies {
+    compile("com.octokat.app:octokat-app:1.0-SNAPSHOT")
+}
